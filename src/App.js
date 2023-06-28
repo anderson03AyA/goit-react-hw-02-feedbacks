0dcea7435327b2a7a3bdb8ffd19211b2f274aa8e
anderson03AyA <andersonsolarte03@gmail.com>
2023-06-28 05:31:09 -0500

    feedback

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Feedback from "./components/Feedback/Feedback";
 
 function App() {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const [goodClicks, setGoodClicks] = useState(0);
+
+  const handleFeedback = (type) => {
+    if (type === "good") {
+      setGoodClicks((prevGoodClicks) => prevGoodClicks + 1);
+    }
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    return Object.values(feedback).reduce((total, value) => total + value, 0);
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const feedbackValues = Object.values(feedback);
+    const positiveFeedbacks = feedbackValues.filter((value) => value > 0);
+    const percentage = (goodClicks / countTotalFeedback()) * 100;
+    return isNaN(percentage) ? 0 : percentage.toFixed(2);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Feedback
+        onFeedback={handleFeedback}
+        data={feedback}
+        total={countTotalFeedback}
+        positive={countPositiveFeedbackPercentage()}
+      />
     </div>
   );
 }
